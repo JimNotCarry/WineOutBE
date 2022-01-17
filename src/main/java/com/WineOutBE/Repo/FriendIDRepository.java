86ac528f18b9b94c9870455b9bf6b3f01016f799
Linus Hellberg <59293465+JimNotCarry@users.noreply.github.com>
2022-01-17 19:15:13 +0100

    Updated gitignore

--- src/main/java/com/WineOutBE/Repo/FriendIDRepository.java
+++
@@ -1,9 +0,0 @@
-package com.WineOutBE.Repo;
-
-import com.WineOutBE.Entity.FriendID;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface FriendIDRepository extends JpaRepository<FriendID, String> {
-}
