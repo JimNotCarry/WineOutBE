03a598dd201a57d905e1660991a10a6d64ef0726
Linus Hellberg <59293465+JimNotCarry@users.noreply.github.com>
2022-02-12 15:41:01 +0100

    Service

--- src/main/java/com/WineOutBE/Service/UserService.java
+++ src/main/java/com/WineOutBE/Service/UserService.java
@@ -1,42 +1,16 @@
 package com.WineOutBE.Service;
 
-import com.WineOutBE.Entity.FriendID;
+import com.WineOutBE.Entity.Role;
 import com.WineOutBE.Entity.User;
-import com.WineOutBE.Repo.FriendIDRepository;
-import com.WineOutBE.Repo.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.http.ResponseEntity;
 
-import java.lang.reflect.Array;
 import java.util.List;
 
-@Service
-public class UserService {
-
-    @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    FriendIDRepository friendIDRepository;
-
-
-    public void createUser(String username, String password, FriendID FID) {
-
-        friendIDRepository.save(FID);
-
-        User user = new User();
-
-        user.setFID(FID);
-
-        user.setUsername(username);
-        user.setPassword(password);
-
-
-        userRepository.save(user);
-
-    }
-
-    public List<User> getAllUsers() {
-        return userRepository.findAll();
-    }
+public interface UserService {
+    User saveUser(User user);
+    Role saveRole(Role role);
+    void addRoleToUser(String username, String roleName);
+    User getUser(String username);
+    List<User> getUsers();
+    Boolean checkUsername(String requestUsername, String credentialUsername);
 }
