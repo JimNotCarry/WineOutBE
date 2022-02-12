79c7197ca62402fad710a9ce41add6f27ca21e09
Linus Hellberg <59293465+JimNotCarry@users.noreply.github.com>
2022-02-12 15:41:38 +0100

    Application

--- src/main/java/com/WineOutBE/WineOutBeApplication.java
+++ src/main/java/com/WineOutBE/WineOutBeApplication.java
@@ -1,8 +1,17 @@
 package com.WineOutBE;
 
+import com.WineOutBE.Entity.Role;
+import com.WineOutBE.Entity.User;
+import com.WineOutBE.Service.UserService;
+import com.WineOutBE.Service.UserServiceImpl;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.ArrayList;
 
 @SpringBootApplication
 public class WineOutBeApplication {
@@ -10,5 +19,31 @@ public class WineOutBeApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(WineOutBeApplication.class, args);
 	}
+
+	//@Bean //Uncomment to add users
+	CommandLineRunner run(UserService userService) {
+		return args -> {
+
+//			userService.saveRole(new Role(null, "ROLE_ADMIN"));
+//			userService.saveRole(new Role(null, "ROLE_USER"));
+//
+//
+//			userService.saveUser(new User(null,"khberg", "pass123", new ArrayList<>()));
+//			userService.saveUser(new User(null, "bellabus", "123pass", new ArrayList<>()));
+//			userService.saveUser(new User(null, "lindz0r", "123pass", new ArrayList<>()));
+			//userService.saveUser(new User(null, "lhberg", "123pass", new ArrayList<>()));
+
+
+//			userService.addRoleToUser("khberg", "ROLE_USER");
+//			userService.addRoleToUser("bellabus", "ROLE_USER");
+//			userService.addRoleToUser("lindz0r", "ROLE_USER");
+			//userService.addRoleToUser("lhberg", "ROLE_ADMIN");
+		};
+	};
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	};
 
 }
