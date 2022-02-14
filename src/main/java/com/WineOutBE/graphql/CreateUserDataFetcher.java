ed6fe970039fbf9f67e1753f0e801dd8128aea23
Linus Hellberg <59293465+JimNotCarry@users.noreply.github.com>
2022-02-14 02:34:38 +0100

    datafetcher createuser update

--- src/main/java/com/WineOutBE/graphql/CreateUserDataFetcher.java
+++ src/main/java/com/WineOutBE/graphql/CreateUserDataFetcher.java
@@ -1,15 +1,20 @@
 package com.WineOutBE.graphql;
 
 import com.WineOutBE.Entity.DiarySettings;
+import com.WineOutBE.Entity.Role;
 import com.WineOutBE.Entity.User;
+import com.WineOutBE.Repo.RoleRepository;
+import com.WineOutBE.Service.DataFetcherServiceImpl;
 import com.WineOutBE.Service.UserService;
 import com.WineOutBE.graphql.InputEntities.UserInput;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import graphql.schema.DataFetcher;
 import graphql.schema.DataFetchingEnvironment;
 import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.Collection;
 
 @Component
 public class CreateUserDataFetcher implements DataFetcher<User> {
@@ -17,24 +22,30 @@ public class CreateUserDataFetcher implements DataFetcher<User> {
     @Autowired
     UserService userService;
 
+    @Autowired
+    DataFetcherServiceImpl dataFetcherService;
+
+    @Autowired
+    RoleRepository roleRepository;
+
     @SneakyThrows
     @Override
     public User get(DataFetchingEnvironment environment) {
 
         User user = new User();
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        String input = objectMapper.writeValueAsString(environment.getArgument("user"));
+        UserInput newuser = dataFetcherService.objectMapper(environment,"user", UserInput.class);
 
-        UserInput newuser = objectMapper.readValue(input,UserInput.class);
+        Collection<Role> roles = new ArrayList<>();
+        roles.add(roleRepository.findByName("User"));
 
         user.setUsername(newuser.getUsername());
         user.setPassword(newuser.getPassword());
         user.setFirstname(newuser.getFirstname());
         user.setLastname(newuser.getLastname());
-        user.setRoles(newuser.getRoles());
-
+        user.setRoles(roles);
         user.setDiarySettings(new DiarySettings());
+        user.setDiaryPost(new ArrayList<>());
 
         return userService.saveUser(user);
     }
