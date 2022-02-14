f0bb9ff7326cb313bfa0d0a1701261520e34d4c8
Linus Hellberg <59293465+JimNotCarry@users.noreply.github.com>
2022-02-14 02:37:15 +0100

    Userservice update

--- src/main/java/com/WineOutBE/Service/UserService.java
+++ src/main/java/com/WineOutBE/Service/UserService.java
@@ -1,5 +1,6 @@
 package com.WineOutBE.Service;
 
+import com.WineOutBE.Entity.DiaryPost;
 import com.WineOutBE.Entity.Role;
 import com.WineOutBE.Entity.User;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,7 @@ import java.util.List;
 
 public interface UserService {
     User saveUser(User user);
+    User updateUser(User user);
     Role saveRole(Role role);
     void addRoleToUser(String username, String roleName);
     User getUser(String username);
